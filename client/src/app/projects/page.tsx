0fc90cf8b02e40331b94f6bd69cb80808eb4442a
YanivRabin <rabin.yaniv@gmail.com>
2025-02-15 10:08:43 -0600

    add scroll to top

--- client/src/app/projects/page.tsx
+++ client/src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ImageGallery from "@/components/ImageGallery";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const images = [
   "/projects/1.jpg",
@@ -16,12 +17,15 @@ const images2 = [
   "/projects/8.jpg",
 ];
 
+
 export default function Projects() {
   
   return (
     <div className="min-h-screen bg-white p-10">
       <ImageGallery title="Area & Path Lighting" images={images} />
       <ImageGallery title="Garden & Landscape Lighting" images={images2} />
+      <ImageGallery title="Garden & Landscape Lighting" images={images2} />
+      <ScrollToTop />
     </div>
   );
 }
